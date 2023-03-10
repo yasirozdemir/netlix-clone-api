c508a258cd91ea5a51ec22cda85c3cb32bd96657
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-03-10 12:16:30 +0300

    adds error handlers

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 import Express from "express";
 import cors from "cors";
 import createHttpError from "http-errors";
+import {
+  badRequestHandler,
+  genericErrorHandler,
+  notFoundHandler,
+  unauthorizedHandler,
+} from "./errorHandlers.js";
 
 const server = Express();
 const port = process.env.PORT || 3001;
@@ -8,7 +14,7 @@ const port = process.env.PORT || 3001;
 const whiteList = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
 server.use(
   cors({
-    origin: (currentOrigin, corseNext) => {
+    origin: (currentOrigin, corsNext) => {
       if (!currentOrigin || whiteList.indexOf(currentOrigin) !== -1) {
         corsNext(null, true);
       } else {
@@ -24,6 +30,11 @@ server.use(
 );
 
 server.use(Express.json());
+
+server.use(badRequestHandler);
+server.use(unauthorizedHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
 
 server.listen(port, () => {
   console.log("Server is running on port:", port);
